96340dc1f86167861d8cca91c737c7a42b517173
enoch-danijel <enochdanijel@gmail.com>
2023-11-14 15:12:13 +0100

    updated web3 and type and abis to SBT

--- sdk/web3.ts
+++ sdk/web3.ts
@@ -1,4 +1,9 @@
-import { NftPass, NftPass__factory } from "@/types/contracts";
+import {
+	NftPass,
+	NftPass__factory,
+	SoulBound,
+	SoulBound__factory,
+} from "@/types/contracts";
 import { Provider, parseEther } from "ethers";
 import { Signer } from "ethers";
 import { AddressLike } from "ethers";
@@ -6,12 +11,17 @@ import { AddressLike } from "ethers";
 export default class GamePassNftContract {
 	SignerFactory: NftPass;
 	signer;
+	SoulBoundfFactory: SoulBound;
 	constructor(nftPassAddress: string, signer: Signer, Provider: Provider) {
 		const getFactoryWithSigner = NftPass__factory.connect(
 			nftPassAddress,
 			signer
 		);
 		this.SignerFactory = getFactoryWithSigner;
+		this.SoulBoundfFactory = SoulBound__factory.connect(
+			nftPassAddress,
+			signer
+		);
 		this.signer = signer;
 	}
 	async BuyMove() {
@@ -23,12 +33,8 @@ export default class GamePassNftContract {
 			}
 		);
 	}
-	async burnNft(_amount: number) {
-		return await this.SignerFactory.burn(
-			await this.signer.getAddress(),
-			1,
-			_amount
-		);
+	async burnNft(_tokenId: number) {
+		return await this.SoulBoundfFactory.burn(_tokenId);
 	}
 	async transferNft(_amount: string, to: string) {
 		return await this.SignerFactory.safeTransferFrom(
@@ -38,5 +44,9 @@ export default class GamePassNftContract {
 			_amount,
 			"0x00"
 		);
+	}
+
+	async claimSBT() {
+		return await this.SoulBoundfFactory.safeMint();
 	}
 }
